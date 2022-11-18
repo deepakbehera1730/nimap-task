6530fde5c5d12050712abed6aaa7c73f2f39d7b4
deepakbehera1722 <118535168+deepakbehera1722@users.noreply.github.com>
2022-11-18 19:14:18 +0530

    Add files via upload

---
+++ Package Creation BY Notepad/Exam.java
@@ -0,0 +1,13 @@
+package pack2;
+import pack1.Student;
+public class Exam{
+public static void main(String args[])
+{
+Student s1=new Student();
+s1.getrollno(200);
+s1.getname("Deepak");
+System.out.println("Student roll no is "+s1.setrollno());
+System.out.println("Student name is "+s1.setname());
+}
+}
+
---
+++ Package Creation BY Notepad/Student.java
@@ -0,0 +1,21 @@
+package pack1;
+public class Student{
+int rollno;
+String Name;
+public void getrollno(int x)
+{
+rollno=x;
+}
+public void getname(String k)
+{
+Name=k;
+}
+public int setrollno()
+{
+return(rollno);
+}
+public String setname()
+{
+return(Name);
+}
+}
