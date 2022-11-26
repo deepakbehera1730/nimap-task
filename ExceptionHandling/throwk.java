8d4a2ffe261e3eda645b19711fef12c679a7e2c7
deepakbehera1722 <118535168+deepakbehera1722@users.noreply.github.com>
2022-11-26 14:04:28 +0530

    Add files via upload

--- ExceptionHandling/throwk.java
+++ ExceptionHandling/throwk.java
@@ -1,6 +1,6 @@
 package ExceptionHandling;
 
-public class throwk {
+public class throwk extends Exception{
       
 	
 	void divisionint(int a,int b)
@@ -9,18 +9,27 @@ public class throwk {
 		if(b==0)
 		{
 			throw new ArithmeticException();
+			
 		}
+		
 		else {
 			c=a/b;
+			
 			System.out.println("Division of ab:"+c);
 		}
+		
 	}
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
 		throwk t1=new throwk();
-		t1.divisionint(9,0);
-
+		try{t1.divisionint(9,0);
+		}
+		catch(ArithmeticException e)
+		{
+			System.out.println(e);
+		}
 	}
 
 }
--- ExceptionHandling/throwsexception.java
+++ ExceptionHandling/throwsexception.java
@@ -1,11 +1,33 @@
 package ExceptionHandling;
 
-public class throwsexception {
-	public static void main (String args[]) throws Exception
-	{
-		String k=null;
-		System.out.println(k.toUpperCase());
-		
+public class throwsexception
+{
+	
+	public  void check() throws ArithmeticException,NullPointerException{
+		int a,b,c;
+		a=10;
+		b=0;
+		c=a/b;
+		String l=null;
+		System.out.println(l.toUpperCase());
 	}
+	   	 
 
+  public static void main(String args[]) 
+  {
+	  throwsexception d=new throwsexception();
+	  try {
+	  d.check();
+	  }
+   catch(ArithmeticException e)
+	  {
+	   System.out.println(e);
+	  }
+	  catch(NullPointerException e)
+	  {
+	   System.out.println(e);
+	  }
+    
+    
+  }
 }
